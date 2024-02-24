c5440dd2c674691e6f20e2e91320cd1870117602
Guillaume Coigniez <120651399+Guillaumecoi@users.noreply.github.com>
2024-02-24 01:00:57 +0000

    Same for boolean

--- PlanPulse/course/tests/models/test_metrics.py
+++ PlanPulse/course/tests/models/test_metrics.py
@@ -1,55 +1,55 @@
 import unittest
 
 from django.forms import ValidationError
-from course.models.metric import Number
+from course.models.metric import Number, Boolean, Time, Percentage
 
 class NumberTest(unittest.TestCase):
     def setUp(self):
         self.number = Number()
 
-    def test_get_positive_value(self):
-        result = self.number.get(5.00)
-        self.assertEqual(result, 5)
-
-    def test_get_negative_value(self):
+    def test_get(self):
+        self.assertEqual(self.number.get(5.00), 5)
         with self.assertRaises(ValidationError):
             self.number.get(-5.00)
 
-    def test_put_positive_value(self):
-        result = self.number.put(5)
-        self.assertEqual(result, 5)
-
-    def test_put_negative_value(self):
+    def test_put(self):
+        self.assertEqual(self.number.put(5), 5)
         with self.assertRaises(ValidationError):
             self.number.put(-5)
 
-    def test_add_positive_values(self):
-        result = self.number.add(2, 3)
-        self.assertEqual(result, 5)
-
-    def test_add_negative_values(self):
+    def test_add(self):
+        self.assertEqual(self.number.add(2, 3), 5)
         with self.assertRaises(ValidationError):
             self.number.add(-2, 3)
-
-        with self.assertRaises(ValidationError):
             self.number.add(2, -3)
-
-        with self.assertRaises(ValidationError):
             self.number.add(-2, -3)
 
-    def test_subtract_positive_values(self):
-        result = self.number.subtract(5, 3)
-        self.assertEqual(result, 2)
-
-    def test_subtract_negative_values(self):
+    def test_subtract(self):
+        self.assertEqual(self.number.subtract(5, 3), 2)
         with self.assertRaises(ValidationError):
             self.number.subtract(-5, 3)
-
-        with self.assertRaises(ValidationError):
             self.number.subtract(5, -3)
-
-        with self.assertRaises(ValidationError):
             self.number.subtract(-5, -3)
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestBoolean(unittest.TestCase):
+    def setUp(self):
+        self.boolean = Boolean()
+
+    def test_get(self):
+        self.assertEqual(self.boolean.get(0), False)
+        self.assertEqual(self.boolean.get(1), True)
+        with self.assertRaises(ValidationError):
+            self.boolean.get(2)
+
+    def test_put(self):
+        self.assertEqual(self.boolean.put(False), 0)
+        self.assertEqual(self.boolean.put(True), 1)
+
+    def test_add(self):
+        with self.assertRaises(ValidationError):
+            self.boolean.add(True, False)
+
+    def test_subtract(self):
+        with self.assertRaises(ValidationError):
+            self.boolean.subtract(True, False)
