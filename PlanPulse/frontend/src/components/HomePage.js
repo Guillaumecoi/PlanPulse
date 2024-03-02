fb015c5189041fe02c19e0802903460936070310
Guillaume Coigniez <120651399+Guillaumecoi@users.noreply.github.com>
2024-03-02 21:48:30 +0000

    React setup

---
+++ PlanPulse/frontend/src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>PlanPulse</h1>
+                <p>PlanPulse is a web application that helps you plan your day and keep track of your tasks.</p>
+            </div>
+        );
+    }
+}
