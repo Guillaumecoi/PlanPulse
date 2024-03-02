37bf6bc9f1339daad6e7d92e1c49f5688c099730
Guillaume Coigniez <120651399+Guillaumecoi@users.noreply.github.com>
2024-03-02 22:27:13 +0000

    create_view

--- PlanPulse/course/views.py
+++ PlanPulse/course/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Course
 
-# Create your views here.
+
+class CreateCourseView(CreateView):
+    '''
+    View for creating a course
+    '''
+    model = Course
+    fields = ['name', 'description', 'institution', 'instructor', 'study_points']
+    template_name = 'course/create_course.html'
+    success_url = reverse_lazy('course:course_list')
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create Course'
+        return context
